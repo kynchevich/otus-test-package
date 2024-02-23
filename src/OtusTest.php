a15f95a85c16c6a7f5012ef6b4ec65e58df7013f
Ivan Kuncevich <dev_ik@myfin.group>
2024-02-23 12:03:13 +0300

    Remove docker-compose

--- src/OtusTest.php
+++ src/OtusTest.php
@@ -1,11 +1,12 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Kynchevich\OtusTestPackage;
 
 class OtusTest
 {
-    public function helloWorld()
+    public function helloWorld():void
     {
         echo "Hellow world this my first package V1.0.1";
     }
